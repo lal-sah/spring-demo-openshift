c4a5c7e163789ff45480f7830c0cda32e49776bd
Lal Sah <sah.lalbabu@gmail.com>
2025-01-01 15:09:48 -0600

    Initial version

---
+++ src/main/java/com/bolidify/springdemoopenshift/SpringDemoOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.bolidify.springdemoopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoOpenshiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoOpenshiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bolidify/springdemoopenshift/SpringDemoOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolidify.springdemoopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoOpenshiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
